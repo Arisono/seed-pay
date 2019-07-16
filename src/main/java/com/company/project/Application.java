10c99b266311c95267df1c74d6590510e4cdede6
Arison <784602719@qq.com>
2019-07-16 19:46:03 +0800

    commit

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -5,17 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-
 import com.company.project.config.StartupRunner;
 import com.company.project.listener.StartListener;
 import com.company.project.web.socket.WebSocket;
+
 
 
 @SpringBootApplication
 public class Application  extends SpringBootServletInitializer{
 	
     public static void main(String[] args) {   	
+    	SpringApplication.run(Application.class, args);
         SpringApplication sa = new SpringApplication(Application.class);
         sa.addListeners(new StartListener());//监听应用启动  其它监听事件在初始化完成之后自动监听
         //解决webSocket注入问题
